4e4287de8feea3a784d9a70550cc452c7cb7fa18
David <david.dekeuwer@gmail.com>
2022-06-24 21:16:09 +0200

    feat: add post method test on NewBill

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -11,24 +11,27 @@ import { localStorageMock } from "../__mocks__/localStorage.js"
 import mockStore from "../__mocks__/store.js"
 
 describe("Given I am connected as an employee", () => {
+    beforeEach(() => {
+        Object.defineProperty(window, "localStorage", {
+            value: localStorageMock,
+        })
+        window.localStorage.setItem(
+            "user",
+            JSON.stringify({
+                type: "Employee",
+                email: "test@gmail.com",
+            })
+        )
+
+        const html = NewBillUI()
+        document.body.innerHTML = html
+
+        const onNavigate = jest.fn()
+
+        const newBill = new NewBill({ document, onNavigate, store: mockStore })
+    })
     describe("When I am on NewBill Page and upload a image", () => {
         test("Then the file input should show file name", async () => {
-            Object.defineProperty(window, "localStorage", {
-                value: localStorageMock,
-            })
-            window.localStorage.setItem(
-                "user",
-                JSON.stringify({
-                    type: "Employee",
-                    email: "test@gmail.com",
-                })
-            )
-
-            const html = NewBillUI()
-            document.body.innerHTML = html
-
-            const newBill = new NewBill({ document, store: mockStore })
-
             const fileInput = document.querySelector(
                 `input[data-testid="file"]`
             )
@@ -41,6 +44,49 @@ describe("Given I am connected as an employee", () => {
                 })
             )
             expect(fileInput.files[0].name).toEqual("test.png")
+        })
+    })
+
+    describe("When I am on NewBill Page and upload a pdf file", () => {
+        test("Then an alert should show and no file will be uploaded", async () => {
+            const spyAlert = jest.spyOn(window, "alert")
+
+            const fileInput = document.querySelector(
+                `input[data-testid="file"]`
+            )
+
+            const file = new File([""], "test.pdf", { type: "application/pdf" })
+
+            await waitFor(() =>
+                fireEvent.change(fileInput, {
+                    target: { files: [file] },
+                })
+            )
+
+            expect(spyAlert).toHaveBeenCalled()
+        })
+    })
+
+    describe("When I am on NewBill Page and submit the form", () => {
+        test("Then an update called must be called", async () => {
+            const spyUpdate = jest.spyOn(mockStore.bills(), "update")
+
+            const fileInput = document.querySelector(
+                `input[data-testid="file"]`
+            )
+
+            const file = new File([""], "test.png", { type: "image/png" })
+
+            await waitFor(() =>
+                fireEvent.change(fileInput, {
+                    target: { files: [file] },
+                })
+            )
+            const form = screen.getByTestId("form-new-bill")
+
+            await waitFor(() => fireEvent.submit(form))
+
+            expect(spyUpdate).toHaveBeenCalled()
         })
     })
 })
