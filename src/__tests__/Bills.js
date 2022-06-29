2f8d0b06e37c662f5bd17986377c1b902779fc2b
David <david.dekeuwer@gmail.com>
2022-06-29 21:36:12 +0200

    fix: click on eye to see bill image test

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -82,15 +82,23 @@ describe("Given I am connected as an employee", () => {
         })
     })
     describe("When I am on Bills Page and click on Icon eye", () => {
-        test("Then I should see the modal", async () => {
+        test("Then I should see the modal with the corresponding image", async () => {
             const root = document.createElement("div")
             root.setAttribute("id", "root")
             document.body.append(root)
+
             router()
             window.onNavigate(ROUTES_PATH.Bills)
 
+            $.fn.modal = jest.fn()
+
             const billIconEye = screen.getAllByTestId("icon-eye")[0]
             billIconEye.click()
+
+            const billImage = screen.getByAltText("Bill")
+            expect(billImage.getAttribute("src")).toBe(
+                "https://test.storage.tld/v0/b/billable-677b6.a…f-1.jpg?alt=media&token=c1640e12-a24b-4b11-ae52-529112e9602a"
+            )
         })
     })
 })
